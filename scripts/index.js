8b63aa44f6ab70bcb66e50b3cc8259cc564a3114
Julia Kubareva <juliakuba27@yandex.ru>
2024-10-31 16:10:37 +0600

    version 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const plasesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard (dataCard, callbackDeleteCard) {
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardItem.querySelector('.card__image').src = dataCard.link;
+  cardItem.querySelector('.card__image').alt = dataCard.alt;
+
+  cardItem.querySelector('.card__title').textContent = dataCard.name;
+
+  cardItem.querySelector('.card__delete-button').addEventListener('click', function () {
+    callbackDeleteCard(cardItem);
+  })
+
+  return cardItem;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard (card) {
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (item) {
+  plasesList.append(createCard(item, deleteCard));
+});
